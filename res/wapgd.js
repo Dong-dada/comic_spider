a1a2f76e5aaa069bab59287e7b56b1a7f708d202
Dong dada <dongyu_1991@outlook.com>
2017-04-11 18:57:15 +0800

    添加 lzstring.js 和 lzstring.py

--- res/wapgd.js
+++
@@ -1,7 +0,0 @@
-﻿document.writeln("<script type=\'text/javascript\'>");
-document.writeln("panshi_a =\'3474178048_3504414720_2\';");
-document.writeln("panshi_b =\'640_96_10\';");
-document.writeln("</script>");
-document.writeln("<script type=\'text/javascript\' src=\'http://s.adyun.com/show_ps3.js\'></script>");
-document.writeln("");
-
